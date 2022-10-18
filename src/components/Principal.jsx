016dc28756684fb5eeba72a9bc7178d87976bce9
david3vc <dveracabanillas@hotmail.com>
2022-10-18 00:38:52 -0500

    media queries

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import imagenPrincipal from '../assets/image-hero-mobile.png'
+import imagenPrincipalMobile from '../assets/image-hero-mobile.png'
+import imagenPrincipalTablet from '../assets/image-hero-tablet.png'
+import imagenPrincipalDesktop from '../assets/image-hero-desktop.png'
 import '../styles/cabecera.css'
 import '../styles/principal.css'
 
@@ -21,7 +23,9 @@ const Principal = () => {
             </div>
         </div>
         <div className="principal__imagen">
-            <img src={imagenPrincipal} alt="" className='principal__imagen-receptiva' />
+            <img src={imagenPrincipalMobile} alt="" className='principal__imagen-receptiva principal__imagen-mobile' />
+            <img src={imagenPrincipalTablet} alt="" className='principal__imagen-tablet' />
+            <img src={imagenPrincipalDesktop} alt="" className='principal__imagen-desktop' />
         </div>
     </div>
   )
